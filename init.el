0ff222ab885f229db9281b0de85a50f244413325
Rafael Palomar <rafael.palomar@rr-research.no>
2019-05-03 22:12:38 +0200

    More changes

--- init.el
+++ init.el
@@ -11,12 +11,17 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/confinit.org"))
 
+ ;; workaround for gnutls problem
+(advice-add 'gnutls-available-p :around #'ignore)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key try use-package))))
+ '(package-selected-packages
+   (quote
+    (org-bullets zerodark-theme zenburn-theme which-key virtualenvwrapper use-package tao-theme poet-theme plantuml-mode moe-theme jedi gist flycheck faff-theme elpy doom-themes doom-modeline counsel company-jedi color-theme-modern cmake-mode base16-theme all-the-icons-ivy all-the-icons-dired alect-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
